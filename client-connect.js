896113418cddbb2c6807767f4abc7d6c895ce7db
Petar Šegina <psegina@ymail.com>
2015-10-18 01:37:01 +0200

    Connect client to server with socket.io

--- client-connect.js
+++ client-connect.js
@@ -1,3 +1,6 @@
+// This global needs to be declared as it is used by the actual script
+GA_SHARE_URL = 'http://localhost:8080';
+
 var s = document.createElement('script');
-s.setAttribute('src', '//localhost:8080/client.js');
+s.setAttribute('src', GA_SHARE_URL + '/client.js');
 document.body.appendChild(s);
--- static/client.js
+++ static/client.js
@@ -1,21 +1,45 @@
-var inputs = document.querySelectorAll('input, textarea, select');
-
-var url = document.URL;
-
-for(var i in inputs) {
-	var input = inputs[i];
-	input.oninput = function() {
-		var event = {
-			url: url,
-			input: {
-				name: this.name,
-				type: this.type,
-				value: this.value
-			}
-		};
-		console.log(event);
-	};
-	input.onchange = input.oninput;
+function loadJS(src, callback) {
+	var s = document.createElement('script');
+	s.src = src;
+	s.async = true;
+	s.onreadystatechange = s.onload = function() {
+		var state = s.readyState;
+		if (!callback.done && (!state || /loaded|complete/.test(state))) {
+			callback.done = true;
+			callback();
+	   }
+   };
+	document.getElementsByTagName('head')[0].appendChild(s);
 }
 
-alert("Hello world!");
+loadJS(GA_SHARE_URL + '/socket.io/socket.io.js', function() {
+	var socket = io(GA_SHARE_URL);
+	socket.on('form-update', function(data) {
+		console.log('Received form-update!');
+		console.log(data);
+	});
+
+	var inputs = document.querySelectorAll('input, textarea, select');
+
+	var url = document.URL;
+
+	for(var i in inputs) {
+		var input = inputs[i];
+		input.oninput = function() {
+			var event = {
+				url: url,
+				input: {
+					name: this.name,
+					type: this.type,
+					value: this.value
+				}
+			};
+			socket.emit('form-update', event);
+			console.log(event);
+		};
+		input.onchange = input.oninput;
+	}
+
+	alert("Hello world!");
+});
+
