c1885cca2625bbdb676f639cb2adee9143ee32a3
Petar Šegina <psegina@ymail.com>
2015-10-18 02:34:33 +0200

    Use protocol relative URL

--- client-connect.js
+++ client-connect.js
@@ -1,5 +1,5 @@
 // This global needs to be declared as it is used by the actual script
-GA_SHARE_URL = 'http://localhost:8080';
+GA_SHARE_URL = '//localhost:8080';
 
 var s = document.createElement('script');
 s.setAttribute('src', GA_SHARE_URL + '/client.js');
